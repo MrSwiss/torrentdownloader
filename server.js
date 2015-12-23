47770432bea794ed95b56c255c5db3a0d3d0627a
bitanath <bitanath@gmail.com>
2015-12-23 21:19:17 +0530

    Added openshift build hooks

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 var torrentController = require('./controllers/torrents.js');
 var dloadController = require('./controllers/download.js');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || process.env.port || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'; 
 
 app.use(bodyParser.urlencoded());
 
